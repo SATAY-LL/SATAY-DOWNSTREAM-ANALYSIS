bc199622769c83d933a361aa92c125c7d85f3f51
Floor <floordolsma@hotmail.com>
2021-10-12 16:01:03 +0200

    converted into funtion

--- src/Neutral_genes_distribution.py
+++ src/Neutral_genes_distribution.py
@@ -10,11 +10,14 @@ from collections import Counter
 import matplotlib.pyplot as plt
 from numpy import trapz
 import numpy as np
-
-#readsforeverygenefile = 'data_step2_Dnrp1_1BREADS_exelled.csv' 
+#
+#def was used to do the things one three different datasets
+#if you dont want to use the funtion, remove def.. & returm(..) and un# following line:
+#readsforeverygenefile = 'data_step2_Dnrp1_1BREADS_exelled.csv'
 def distribution_neutralgenes(readsforeverygenefile):
     neutralgenes = []
     positivegenes = []
+    #in the following for loop, cellmap data is used to define if genes are neutral or positive
     SMFitness = pd.read_excel(r"C:\Users\floor\OneDrive\Documenten\MASTER\MEP\codes\LaanLab-SATAY-DataAnalysis\Python_scripts\strain_ids_and_single_mutant_fitness.xlsx")
     for i in range(0,len(SMFitness)):
         if SMFitness.at[i,'Single mutant fitness (26°)'] <1.005 and SMFitness.at[i,'Single mutant fitness (26°)'] >0.995 : #nd SMFitness.at[i,'Single mutant fitness (26°)'] <1.010
@@ -27,12 +30,15 @@ def distribution_neutralgenes(readsforeverygenefile):
     tabel['reads']= ''
     tabel['reads']= tabel['reads'].astype('object')
     
+    #open datafile
     with open(readsforeverygenefile, newline='') as f:
         reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
         data = list(reader)
         tabel['reads'] = data
     del(data, reader)
     
+    
+    #the following forloop collects the number of reads out of the datafile from the neutral and positive genes we just defined
     neutral_reads = [ ]
     positive_reads = []
     mean_per_gene_neutral=[]
@@ -41,24 +47,32 @@ def distribution_neutralgenes(readsforeverygenefile):
     mean_all_reads = []
     minimal_insertions = 0
     for i in range(0,len(tabel)):
+        #neutral
         if tabel.at[i,"gene"] in neutralgenes and len(tabel.at[i,'reads'])>minimal_insertions:
             neutral_reads.append(tabel.at[i,"reads"])
-            mean_per_gene_neutral.append(int(np.mean(tabel.at[i,"reads"])))
+            mean_per_gene_neutral.append(int(np.mean(tabel.at[i,"reads"]))) #compute average over each gene/change to np.median
+        #positive
         elif tabel.at[i,"gene"] in positivegenes and len(tabel.at[i,'reads'])>minimal_insertions:
             positive_reads.append(tabel.at[i,"reads"])
-            mean_per_gene_pos.append(np.mean(tabel.at[i,"reads"]))
+            mean_per_gene_pos.append(np.mean(tabel.at[i,"reads"])) #compute average over each gene/change to np.median
+       #all genes
         if  len(tabel.at[i,'reads'])>0:
             all_reads.append(tabel.at[i,"reads"])
-            mean_all_reads.append(int(np.mean(tabel.at[i,"reads"])))
-    avr_over_neutral_genes= np.mean(mean_per_gene_neutral)
+            mean_all_reads.append(int(np.mean(tabel.at[i,"reads"]))) #compute average over each gene/change to np.median
+    
+    #compute total average
+    avr_over_neutral_genes= np.mean(mean_per_gene_neutral) 
     avr_over_pos_genes = np.mean(mean_per_gene_pos)
     avr_over_all_genes = np.mean(mean_all_reads)
     
+    #it is necessary to create a flatlist
+    #neutral
     neutral_flat_list_ = [item for sublist in neutral_reads for item in sublist]
     neutral_totalins = len(neutral_flat_list_)                  
     neutral_totalreads = sum(neutral_flat_list_)
     avrRperI_neutral = neutral_totalreads/ neutral_totalins
     
+    #all genes
     flat_list = [item1 for sublist1 in all_reads for item1 in sublist1]
     totalins= len(flat_list)
     totalreads= sum(flat_list)
@@ -72,24 +86,29 @@ def distribution_neutralgenes(readsforeverygenefile):
     neutral_abundance_reads.columns = ["number of reads","abundance"]
     abN =  neutral_abundance_reads.sort_values(by ='number of reads' )
     
+    
+    #create xdata and ydata 
+    #neutral
     x_data = abN['number of reads'].reset_index(drop=True)#.truncate(0, 94, copy = False)
     y_data = abN['abundance'].reset_index(drop=True)#.truncate(0,94, copy = False)
     areaN= trapz(y_data, dx=1)
     tot_abundance_reads = pd.DataFrame.from_dict(tot_abundance_reads_counter, orient='index').reset_index()
     tot_abundance_reads.columns = ["number of reads","abundance"]
     abT =  tot_abundance_reads.sort_values(by ='number of reads' )
+      #create xdata and ydata 
+      #alln genes
     x_data_TOT = abT['number of reads'].reset_index(drop=True)#.truncate(0, 187, copy = False)
     y_data_TOT = abT['abundance'].reset_index(drop=True)#.truncate(0, 187, copy = False);
     areaT = trapz(y_data_TOT, dx=1)
     y_data = y_data/areaN
     y_data_TOT = y_data_TOT/areaT
     return(x_data, y_data, x_data_TOT, y_data_TOT, avr_over_neutral_genes, avr_over_pos_genes, avrRperI_tot, totalreads)
-
+#do this for datafiles: output of allingment to yeastmine data.
 x_data2, y_data2, x_data_TOT2, y_data_TOT2, avr_over_neutral_genes2, avr_over_pos_genes2, TA, totalA = distribution_neutralgenes('data_step2_Dnrp1_1aREADSexcelled.csv')
 x_data1, y_data1, x_data_TOT1, y_data_TOT1, avr_over_neutral_genes1, avr_over_pos_genes1, TB, totalB = distribution_neutralgenes('data_step2_Dnrp1_1BREADS_exelled.csv')
 x_dataM, y_dataM, x_data_TOTM, y_data_TOTM, avr_over_neutral_genesM, avr_over_pos_genesM, TM, totalM = distribution_neutralgenes('data_step2_Dnrp1_1abmergedREADSexcelled.csv')
 
-#plots
+#create plots
 fig, ax = plt.subplots()
 
 plt.style.use('Solarize_Light2')
